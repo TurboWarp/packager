6556300bf9817462122e600aa620b806d2b40c40
Thomas Weber <weber_t@mailbox.org>
2021-05-27 18:19:15 -0500

    Update TurboWarp

--- packager.js
+++ packager.js
@@ -56,7 +56,7 @@ window.Packager = (function() {
 
   const assetManifest = {
     'turbowarp': {
-      src: 'https://packagerdata.turbowarp.org/packager.ffdb935ef7b7f24aa4c7.js',
+      src: 'https://packagerdata.turbowarp.org/packager.e212db007a686ee5a5c1.js',
       size: 3040000 // TODO get accurate number, it's complicated because of gzip
     },
     'nwjs-win64': {
