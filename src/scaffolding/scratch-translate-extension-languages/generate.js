d70364d042a75131a41b784d7b8a770675049231
Thomas Weber <weber_t@mailbox.org>
2021-06-15 13:36:53 -0500

    Fix scratch-translate-extension-languages shim errors

--- src/scaffolding/scratch-translate-extension-languages/generate.js
+++ src/scaffolding/scratch-translate-extension-languages/generate.js
@@ -6,6 +6,6 @@ const menuMap = languageNames.menuMap;
 for (const key of Object.keys(menuMap)) {
   menuMap[key] = [{code: '', name: ''}];
 }
-languageNames.spokenLanguages = {};
+delete languageNames.spokenLanguages;
 
 fs.writeFileSync(path.join(__dirname, 'languages.json'), JSON.stringify(languageNames));
