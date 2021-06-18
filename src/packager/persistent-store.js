4cdda1858fb24dc8848cd8ccd9c1b26d05fee4b7
Thomas Weber <weber_t@mailbox.org>
2021-06-18 00:34:01 -0500

    Smarter localStorage serialization

--- src/packager/persistent-store.js
+++ src/packager/persistent-store.js
@@ -1,29 +1,21 @@
 import {writable} from 'svelte/store';
 import merge from './lib/merge';
-
-const serialize = (value) => {
-  // Don't try to serialize files or blobs
-  if (value instanceof Blob) {
-    return null;
-  }
-  if (typeof value === 'object' && value !== null) {
-    value = Object.assign({}, value);
-    for (const key of Object.keys(value)) {
-      value[key] = serialize(value[key]);
-    }
-  }
-  return value;
-};
+import serialize from './lib/serialize';
 
 const writablePersistentStore = (key, defaultValue) => {
-  let value = defaultValue;
+  let value = JSON.parse(JSON.stringify(defaultValue));
   const localValue = JSON.parse(localStorage.getItem(key));
   if (localValue) {
-    value = merge(localValue, defaultValue);
+    value = merge(localValue, value);
   }
   const store = writable(value, () => {
     const unsubscribe = store.subscribe(value => {
-      localStorage.setItem(key, JSON.stringify(serialize(value)));
+      const serialized = serialize(value, defaultValue);
+      if (serialized === null) {
+        localStorage.removeItem(key);
+      } else {
+        localStorage.setItem(key, JSON.stringify(serialized));
+      }
     });
     return unsubscribe;
   });
