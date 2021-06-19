1e4027de10c73652194734c875b1a03cd8792976
Thomas Weber <weber_t@mailbox.org>
2021-06-19 17:01:27 -0500

    Make preview use same meta viewport as packager

--- src/packager/preview.js
+++ src/packager/preview.js
@@ -3,7 +3,7 @@ const source = `<!DOCTYPE html>
 <head>
   <title>Loading Preview</title>
   <meta charset="utf8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
   <style>
   body {
     background: black;
