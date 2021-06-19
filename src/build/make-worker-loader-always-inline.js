97c376f236087e01ef728ccd5c9b0cf0bf1dc878
Thomas Weber <weber_t@mailbox.org>
2021-06-18 19:56:38 -0500

    Only force workers from node_modules to be inline

--- src/build/make-worker-loader-always-inline.js
+++ src/build/make-worker-loader-always-inline.js
@@ -1,6 +1,10 @@
 const wrapWorkerLoader = (WorkerLoader) => {
   const pitch = WorkerLoader.pitch;
   WorkerLoader.pitch = function wrapped (...args) {
+    // Only affect the workers in scratch-storage and scratch-vm, not ours.
+    if (!this.resourcePath.includes('node_modules')) {
+      return pitch.call(this, ...args);
+    }
     // We want to redefine this.query, but it's a readonly unconfigurable property.
     // So, we can change what `this` means instead.
     const newThis = new Proxy(this, {
