47d7d499690f332c4b084d155f9e6a6bae00666f
Thomas Weber <weber_t@mailbox.org>
2021-06-18 20:11:37 -0500

    Disable Scratch minilog

--- src/packager/large-dependencies.js
+++ src/packager/large-dependencies.js
@@ -1,6 +1,9 @@
 import VirtualMachine from 'scratch-vm';
 import Storage from 'scratch-storage';
 import JSZip from 'jszip';
+import Minilog from 'minilog';
+
+Minilog.disable();
 
 export {
   VirtualMachine,
