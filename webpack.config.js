b885bd2311a820e9eef151ba01969ae4684c09d8
Thomas Weber <weber_t@mailbox.org>
2021-06-22 12:20:20 -0500

    Fix SCAFFOLDING_BUILD_ID

--- webpack.config.js
+++ webpack.config.js
@@ -68,7 +68,8 @@ module.exports = {
     }),
     new webpack.DefinePlugin({
       'process.env.LARGE_ASSET_BASE': JSON.stringify(process.env.LARGE_ASSET_BASE || 'https://packagerdata.turbowarp.org/'),
-      'process.env.SCAFFOLDING_BUILD_ID': process.env.NODE_ENV === 'production' ? require('./src/build/generate-scaffolding-build-id') : 'Math.random()',
+      // In production mode, generate a unique hash. In development mode, use a random ID every reload as those files are coming from localhost anyways.
+      'process.env.SCAFFOLDING_BUILD_ID': process.env.NODE_ENV === 'production' ? JSON.stringify(require('./src/build/generate-scaffolding-build-id')) : 'Math.random()',
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
     }),
     new HtmlWebpackPlugin({
