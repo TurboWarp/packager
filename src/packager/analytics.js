64ae7f90a76059e8d133871d1786e51aa00a421f
Thomas Weber <weber_t@mailbox.org>
2021-06-24 14:12:51 -0500

    Check hostnames matches PLAUSIBLE_DOMAIN

--- src/packager/analytics.js
+++ src/packager/analytics.js
@@ -4,8 +4,7 @@ const enabled =
   PLAUSIBLE_API &&
   PLAUSIBLE_DOMAIN &&
   (location.protocol === 'http:' || location.protocol === 'https:') &&
-  // once we deploy to the right domain we should also domain
-  // location.hostname === PLAUSIBLE_DOMAIN &&
+  location.hostname === PLAUSIBLE_DOMAIN &&
   navigator.doNotTrack !== '1';
 
 const sendEvent = eventName => {
