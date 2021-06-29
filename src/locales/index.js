91a0e210b49c97e0694db9fa0b1c57744800bf92
Thomas Weber <weber_t@mailbox.org>
2021-06-29 18:29:16 -0500

    Allow adding context to strings

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,5 +1,21 @@
 import en from './en.json';
 
-export default {
-  en
+const unstructure = (messages) => {
+  for (const key of Object.keys(messages)) {
+    const value = messages[key];
+    if (typeof value === 'object') {
+      if (value.string) {
+        messages[key] = value.string;
+      } else {
+        unstructure(value);
+      }
+    }
+  }
+  return messages;
+}
+
+const messages = {
+  en: unstructure(en)
 };
+
+export default messages;
