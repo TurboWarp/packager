179d0aeedeea2951d4cecb5d79fd4a468688e8ca
Thomas Weber <weber_t@mailbox.org>
2021-06-29 18:50:50 -0500

    Update translations

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,5 +1,3 @@
-import en from './en.json';
-
 const unstructure = (messages) => {
   for (const key of Object.keys(messages)) {
     const value = messages[key];
@@ -12,10 +10,30 @@ const unstructure = (messages) => {
     }
   }
   return messages;
-}
+};
 
 const messages = {
-  en: unstructure(en)
+  en: unstructure(require('./en.json')),
+  // Generated code:
+  /*===*/
+  "de": require("./de.json"),
+  "es": require("./es.json"),
+  "fi": require("./fi.json"),
+  "fr": require("./fr.json"),
+  "it": require("./it.json"),
+  "ja": require("./ja.json"),
+  "ko": require("./ko.json"),
+  "nb": require("./nb.json"),
+  "nl": require("./nl.json"),
+  "pl": require("./pl.json"),
+  "pt": require("./pt.json"),
+  "ro": require("./ro.json"),
+  "ru": require("./ru.json"),
+  "sl": require("./sl.json"),
+  "tr": require("./tr.json"),
+  "zh-cn": require("./zh-cn.json"),
+  "zh-tw": require("./zh-tw.json"),
+  /*===*/
 };
 
 export default messages;
