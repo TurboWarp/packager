f182cc13091aec17a41115e962c3b9ef9359c14c
Thomas Weber <weber_t@mailbox.org>
2021-07-07 16:11:44 -0500

    Fix gamepad addon images

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,7 @@ module.exports = {
       },
       {
         test: /\.png$/i,
-        include: /packager/,
+        include: /src[\/\\]packager/,
         use: [
           {
             loader: 'file-loader',
