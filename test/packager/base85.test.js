7a1e0b854edb4fb1f916c4106e820410201ab547
Thomas Weber <weber_t@mailbox.org>
2021-08-04 13:51:02 -0500

    Add base85 test

---
+++ test/packager/base85.test.js
@@ -0,0 +1,13 @@
+import {encode, decode} from '../../src/packager/lib/base85-encode';
+
+test('base85 encode and decode', () => {
+  for (let i = 0; i < 10; i++) {
+    const view = new Uint8Array(Math.random() * 10000);
+    for (let i = 0; i < view.length; i++) {
+      view[i] = Math.random() * 255;
+    }
+    const encoded = encode(view.buffer.slice(view.byteOffset, view.byteLength));
+    const decoded = decode(encoded);
+    expect(decoded).toEqual(view);
+  }
+});
