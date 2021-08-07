9399614b44c8c60654088b119145278249961a1d
Thomas Weber <weber_t@mailbox.org>
2021-08-07 15:57:22 -0500

    Log when scaffolding is not running in a production environment

--- src/scaffolding/export.js
+++ src/scaffolding/export.js
@@ -4,4 +4,8 @@ if (window.Scaffolding) {
   throw new Error('Scaffolding already exists on this page');
 }
 
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Not a production build. Set NODE_ENV to production and rebuild for improved file size and performance (This message will go away).');
+}
+
 window.Scaffolding = Scaffolding;
