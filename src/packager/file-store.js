dedb325de442f9b2fc8c83822ccb9e376427a0de
Thomas Weber <weber_t@mailbox.org>
2021-08-20 14:35:43 -0500

    Fix "NotFoundError: Node was not found" when reading files in Firefox
    Firefox would throw this spuriously when reading some files from IDB as URLs
    This is fixed by always storing these files as ArrayBuffer instead of File

--- src/packager/file-store.js
+++ src/packager/file-store.js
@@ -1,11 +1,23 @@
 import {writable} from 'svelte/store';
 import Database from './lib/idb';
+import {readAsArrayBuffer} from './lib/readers';
 
 const DATABASE_NAME = 'p4-local-settings';
 const DATABASE_VERSION = 1;
 const STORE_NAME = 'blobs';
 
 const db = new Database(DATABASE_NAME, DATABASE_VERSION, STORE_NAME);
+
+const makeNamedBlob = (buffer, type, name) => {
+  const blob = new Blob([buffer], {type});
+  blob.name = name;
+  return blob;
+};
+
+const cloneBlob = async (blob) => {
+  const buffer = await readAsArrayBuffer(blob);
+  return makeNamedBlob(buffer, blob.type, blob.name);
+};
 
 const get = async (key) => {
   const {transaction, store} = await db.createTransaction('readonly');
@@ -15,7 +27,14 @@ const get = async (key) => {
     request.onsuccess = (e) => {
       const result = e.target.result;
       if (result) {
-        resolve(result.data);
+        const data = result.data;
+        // Older versions stored these files as instances of File
+        if (data instanceof Blob) {
+          // Clone immediately to fix spurious "NotFoundError: Node was not found" in Firefox
+          resolve(cloneBlob(data));
+        } else {
+          resolve(makeNamedBlob(data, result.type, result.name));
+        }
       } else {
         resolve(null);
       }
@@ -23,13 +42,16 @@ const get = async (key) => {
   });
 };
 
-const set = async (key, value) => {
+const set = async (key, file) => {
+  const arrayBuffer = file ? await readAsArrayBuffer(file) : null;
   const {transaction, store} = await db.createTransaction('readwrite');
   return new Promise((resolve, reject) => {
     Database.setTransactionErrorHandler(transaction, reject);
-    const request = value ? store.put({
+    const request = file ? store.put({
       id: key,
-      data: value
+      data: arrayBuffer,
+      type: file.type,
+      name: file.name
     }) : store.delete(key);
     request.onsuccess = () => {
       resolve();
