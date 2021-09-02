315c075ee032e6faaf5e692d1608f1066506efaf
Thomas Weber <weber_t@mailbox.org>
2021-09-02 13:18:31 -0500

    SECURITY: sanitize URLs in advanced cloud addon
    Thanks @apple502j

--- src/addons/special-cloud-behaviors/index.js
+++ src/addons/special-cloud-behaviors/index.js
@@ -1,3 +1,12 @@
+const isSafeURL = (url) => {
+  try {
+    const u = new URL(url, location.href);
+    return u.protocol === 'http:' || u.protocol === 'https:' || u.protocol === 'data:';
+  } catch (e) {
+    return false;
+  }
+};
+
 class SpecialCloudBehaviorsProvider {
   enable () {
     this.manager.setVariable(this, '☁ url', location.href);
@@ -5,10 +14,14 @@ class SpecialCloudBehaviorsProvider {
 
   handleUpdateVariable (name, value) {
     if (name === '☁ redirect') {
-      location.href = value;
+      if (isSafeURL(value)) {
+        location.href = value;
+      }
     } else if (name === '☁ open link') {
-      // Unreliable
-      window.open(value);
+      if (isSafeURL(value)) {
+        // Unreliable
+        window.open(value);
+      }
     } else if (name === '☁ username') {
       this.manager.parent.setUsername(value);
     }
