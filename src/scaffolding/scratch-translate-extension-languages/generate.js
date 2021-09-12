eedd5e01635d71efae25fa40504b5185e8cbcdf2
Thomas Weber <weber_t@mailbox.org>
2021-09-11 20:03:31 -0500

    Fix language block

--- src/scaffolding/scratch-translate-extension-languages/generate.js
+++ src/scaffolding/scratch-translate-extension-languages/generate.js
@@ -3,8 +3,9 @@ const path = require('path');
 
 const languageNames = require('scratch-translate-extension-languages');
 const menuMap = languageNames.menuMap;
-for (const key of Object.keys(menuMap)) {
-  menuMap[key] = [{code: '', name: ''}];
+for (const languageCode of Object.keys(menuMap)) {
+  // Keep a language's own translation as its used by the "language" block.
+  menuMap[languageCode] = menuMap[languageCode].filter(({code}) => code === languageCode);
 }
 delete languageNames.spokenLanguages;
 
