1b5d534d4ae5b36f80b7faf156d1176a2c7a114c
Thomas Weber <weber_t@mailbox.org>
2021-09-24 22:05:48 -0500

    Rewrite StandalonePlugin to be less invasive

--- src/build/standalone-plugin.js
+++ src/build/standalone-plugin.js
@@ -1,13 +1,21 @@
+// Plugin to modify webpack to interpret all dynamic imports as webpackMode: "eager"
+
+const patchParser = (parser) => {
+  const originalParseCommentOptions = parser.parseCommentOptions;
+  parser.parseCommentOptions = function (...args) {
+    const result = originalParseCommentOptions.call(this, ...args);
+    result.options.webpackMode = 'eager';
+    return result;
+  };
+};
+
 class TWStandalonePlugin {
-  apply () {
-    // Modify webpack to interpret all dynamic imports as webpackMode: "eager"
-    const Parser = require('webpack/lib/Parser');
-    const originalParseCommentOptions = Parser.prototype.parseCommentOptions;
-    Parser.prototype.parseCommentOptions = function (...args) {
-      const result = originalParseCommentOptions.call(this, ...args);
-      result.options.webpackMode = 'eager';
-      return result;
-    };
+  apply (compiler) {
+    compiler.hooks.normalModuleFactory.tap('TWStandalonePlugin', (normalModuleFactory) => {
+      normalModuleFactory.hooks.parser.for('javascript/auto').tap('TWStandalonePlugin', patchParser);
+      normalModuleFactory.hooks.parser.for('javascript/dynamic').tap('TWStandalonePlugin', patchParser);
+      normalModuleFactory.hooks.parser.for('javascript/esm').tap('TWStandalonePlugin', patchParser);
+    });
   }
 }
 
