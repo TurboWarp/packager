cd98d35a94b26347b2be89a5a270d697d854f566
Thomas Weber <weber_t@mailbox.org>
2021-10-02 21:02:46 -0500

    Fix translate extension crash in es-419, pt-br, ja-Hira
    This should fix #179

--- src/scaffolding/scratch-translate-extension-languages/generate.js
+++ src/scaffolding/scratch-translate-extension-languages/generate.js
@@ -3,9 +3,17 @@ const path = require('path');
 
 const languageNames = require('scratch-translate-extension-languages');
 const menuMap = languageNames.menuMap;
-for (const languageCode of Object.keys(menuMap)) {
-  // Keep a language's own translation as its used by the "language" block.
-  menuMap[languageCode] = menuMap[languageCode].filter(({code}) => code === languageCode);
+for (const [languageCode, languageMenu] of Object.entries(menuMap)) {
+  const keepLanguages = [
+    languageCode,
+    languageCode.split('-')[0]
+  ];
+  const newMenu = languageMenu.filter(({code}) => keepLanguages.includes(code));
+  // Empty list can cause crashes at runtime
+  if (newMenu.length === 0) {
+    throw new Error('menuMap is empty for ' + languageCode);
+  }
+  menuMap[languageCode] = newMenu;
 }
 delete languageNames.spokenLanguages;
 
