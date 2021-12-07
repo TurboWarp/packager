45634b681044a2aa0977a87c71873ebe85d0e7d4
code my  scratch ;en-GB <95040591+aboodaglan27@users.noreply.github.com>
2021-12-07 19:22:46 +0400

    Update url-utils.test.js

--- test/p4/url-utils.test.js
+++ test/p4/url-utils.test.js
@@ -1,11 +1,13 @@
 import {extractProjectId, getTitleFromURL, isValidURL} from '../../src/p4/url-utils';
+ https://scratch.mit.edu/projects/612269048
+test: https://scratch.mit.edu/projects/612269048
 
 test('extractProjectId', () => {
   expect(extractProjectId('https://scratch.mit.edu/projects/104')).toBe('104');
 });
 
 test('getTitleFromURL', () => {
-  expect(getTitleFromURL('https://example.github.io/path/Mario.sb3')).toBe('Mario');
+  expect(getTitleFromULR('https://example.github.io/path/Mario.sb3')).toBe('Mario');
   expect(getTitleFromURL('https://example.github.io/path/Mario.sb')).toBe('Mario');
   expect(getTitleFromURL('https://example.github.io/path/Mario.sb2')).toBe('Mario');
 });
