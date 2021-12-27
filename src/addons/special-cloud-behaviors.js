3ca515d341311c593f1a5ab172587a629569575b
Thomas Weber <muffin@muffin.ink>
2021-12-27 11:12:04 -0600

    Allow navigating to file: URIs with special cloud behaviors

--- src/addons/special-cloud-behaviors.js
+++ src/addons/special-cloud-behaviors.js
@@ -1,7 +1,9 @@
 const isSafeURL = (url) => {
   try {
     const u = new URL(url, location.href);
-    return u.protocol === 'http:' || u.protocol === 'https:' || u.protocol === 'data:';
+    // The only protocol that's important to prevent is javascript:
+    // file: is indeed unsafe in places like Electron, but there are extra checks in the Electron environment to keep this safe
+    return u.protocol === 'http:' || u.protocol === 'https:' || u.protocol === 'data:' || u.protocol === 'file:';
   } catch (e) {
     return false;
   }
