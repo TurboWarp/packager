98667c7022f0ce243fba2bcbc3887cb781328ce0
Thomas Weber <muffin@muffin.ink>
2022-01-22 19:47:56 -0600

    Remove default cloud variable server update message

--- src/p4/PackagerOptions.svelte
+++ src/p4/PackagerOptions.svelte
@@ -525,13 +525,6 @@
             {$_('options.cloudVariablesHost')}
             <input type="text" bind:value={$options.cloudVariables.cloudHost}>
           </label>
-          <!-- TODO: Remove mid January 2021 -->
-          {#if $options.cloudVariables.cloudHost === 'wss://clouddata.turbowarp.org'}
-            <p class="small">
-              Another update: The packager will try to connect to clouddata.turbowarp.org first, and if that fails, it will try clouddata.turbowarp.xyz instead.
-              This should make connections more reliable on filtered internet connections.
-            </p>
-          {/if}
         </div>
       {/if}
 
