e43b61725a5c0905682827a327227ff00eabb34a
Thomas Weber <muffin@muffin.ink>
2022-03-29 14:49:10 -0500

    Update errors

--- src/common/errors.js
+++ src/common/errors.js
@@ -1,10 +1,16 @@
 /**
- * A type of Error that indicates that the user has configured something wrong, and this error is not a bug.
+ * Error indicating the user has misconfigured something.
  */
 export class UserError extends Error {
-
+  constructor (message) {
+    super(message);
+    this.name = 'UserError';
+  }
 }
 
+/**
+ * Error indicating a process was aborted.
+ */
 export class AbortError extends Error {
   constructor (message) {
     super(message || 'Aborted. Although this looks like a scary error, it\'s perfectly normal if you interrupted a loading bar.');
@@ -12,13 +18,23 @@ export class AbortError extends Error {
   }
 }
 
+/**
+ * Error indicating a network request was unsuccessful and the reason is unknown.
+ */
 export class UnknownNetworkError extends Error {
   constructor (url) {
-    super(`Could not fetch ${url}`);
+    super(`Could not fetch ${url} for unknown reason.`);
+    this.name = 'UnknownNetworkError';
     this.url = url;
   }
 }
 
+/**
+ * Error indicating the packager is outdated and must be updated before it can continue.
+ */
 export class OutdatedPackagerError extends Error {
-
+  constructor (message) {
+    super(message);
+    this.name = 'OutdatedPackagerError';
+  }
 }
