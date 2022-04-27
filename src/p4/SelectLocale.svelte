29120fb154657078809f3ab1471de9027fd00f70
Thomas Weber <muffin@muffin.ink>
2022-04-27 17:48:37 -0500

    Simplify structure of locale-names.json

--- src/p4/SelectLocale.svelte
+++ src/p4/SelectLocale.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <select bind:value={$locale}>
-  {#each Object.entries(supportedLocales) as [locale, {name}]}
+  {#each Object.entries(supportedLocales) as [locale, name]}
     <option value={locale}>{name}</option>
   {/each}
 </select>
