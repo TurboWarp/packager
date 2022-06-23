06257185b3b6db83bb6da69e3024b371579f0539
Thomas Weber <muffin@muffin.ink>
2022-06-23 11:29:38 -0500

    Update pause addon with similar changes as https://github.com/ScratchAddons/ScratchAddons/pull/4697
    Fixes major performance cliff in some projects.
    I only included pause changes; there is no single stepping here, which matters for licensing reasons.

--- src/addons/pause.js
+++ src/addons/pause.js
@@ -2,6 +2,9 @@ export default function ({ scaffolding }) {
   // This file is based on the `pause` addon from Scratch Addons
   // All major portions of this file were written by me, so the GPL does not apply
 
+  const STATUS_PROMISE_WAIT = 1;
+  const STATUS_DONE = 4;
+
   const vm = scaffolding.vm;
 
   let paused = false;
@@ -23,19 +26,7 @@ export default function ({ scaffolding }) {
             status: thread.status,
           };
           pausedThreadState.set(thread, pauseState);
-          // Make sure that paused threads will always be paused.
-          // Setting thread.status is not enough for blocks like "ask and wait"
-          Object.defineProperty(thread, "status", {
-            get() {
-              return /* STATUS_PROMISE_WAIT */ 1;
-            },
-            set(status) {
-              // Status will be set when the thread is unpaused.
-              pauseState.status = status;
-            },
-            configurable: true,
-            enumerable: true,
-          });
+          thread.status = STATUS_PROMISE_WAIT;
         }
       }
 
@@ -60,18 +51,37 @@ export default function ({ scaffolding }) {
             const dt = now - pauseState.pauseTime;
             thread.timer.startTime += dt;
           }
-          Object.defineProperty(thread, "status", {
-            value: pauseState.status,
-            configurable: true,
-            enumerable: true,
-            writable: true,
-          });
+          thread.status = pauseState.status;
         }
       }
       pausedThreadState = new WeakMap();
     }
 
     vm.emit('P4_PAUSE', paused);
+  };
+
+  const ensurePausedThreadIsStillPaused = (thread) => {
+    if (thread.status === STATUS_DONE) {
+      return;
+    }
+    const pauseState = pausedThreadState.get(thread);
+    if (pauseState) {
+      if (thread.status !== STATUS_PROMISE_WAIT) {
+        // We'll record the change so we can properly resume the thread, but the thread must still be paused for now.
+        pauseState.status = thread.status;
+        thread.status = STATUS_PROMISE_WAIT;
+      }
+    }
+  };
+
+  const originalStepThreads = vm.runtime.sequencer.stepThreads;
+  vm.runtime.sequencer.stepThreads = function () {
+    if (paused) {
+      for (const thread of this.runtime.threads) {
+        ensurePausedThreadIsStillPaused(thread);
+      }
+    }
+    return originalStepThreads.call(this);
   };
 
   const originalGreenFlag = vm.runtime.greenFlag;
