cba7a0715b192457d3de71735cc63730ced9a69e
Joshpinto6 <91392083+joshpinto6@users.noreply.github.com>
2022-07-25 12:13:06 -0400

    Update PackagerOptions.svelte

--- src/p4/PackagerOptions.svelte
+++ src/p4/PackagerOptions.svelte
@@ -148,7 +148,7 @@
     var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify($options));
     var dlAnchorElem = document.getElementById('downloadAnchorElem');
     dlAnchorElem.setAttribute("href",     dataStr     );
-    dlAnchorElem.setAttribute("download", "scene.json");
+    dlAnchorElem.setAttribute("download", "turbowarp-packager-settings.json");
     dlAnchorElem.click();
   };
     
