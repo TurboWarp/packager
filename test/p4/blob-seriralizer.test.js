63b64853ebae6b13853b22c399cf729ce887d5e1
GarboMuffin <muffin@muffin.ink>
2022-07-30 23:48:29 -0500

    Add blob serializer tests

---
+++ test/p4/blob-seriralizer.test.js
@@ -0,0 +1,27 @@
+import {recursivelyDeserializeBlobs, recursivelySerializeBlobs} from "../../src/p4/blob-serializer";
+
+global.Blob = class {};
+
+test('basic cloning functionality', async () => {
+  const object = {
+    a: {
+      b: [
+        {
+          c: 3,
+          d: "test"
+        }
+      ]
+    },
+    e: true
+  };
+  const serialized = await recursivelySerializeBlobs(object);
+  const deserialized = recursivelyDeserializeBlobs(serialized);
+  expect(serialized).toEqual(object);
+  expect(deserialized).toEqual(object);
+});
+
+test('cant serialize __isBlob', async () => {
+  await expect(recursivelySerializeBlobs({
+    __isBlob: true
+  })).rejects.toEqual(new Error(`Can't serialize special key: __isBlob`));
+});
