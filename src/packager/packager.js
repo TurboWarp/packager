d7332a1ac322bf5bdffdd1a42d9dac014af76ca7
GarboMuffin <muffin@mailbox.org>
2022-09-11 01:07:48 -0500

    Use "Control+click" instead of "right click" in macOS instructions
    Most macOS devices don't really have a "right click"

--- src/packager/packager.js
+++ src/packager/packager.js
@@ -153,7 +153,7 @@ const generateMacReadme = (options) => `When you try to double click on the app 
 This is normal. Press cancel.
 
 To run the app:
-1) Right click on the app file (${options.app.packageName} in the same folder as this document) and select "Open".
+1) Control+click on the app file (${options.app.packageName} in the same folder as this document) and select "Open".
 2) If a warning appears, select "Open" if it's an option.
 3) If a warning appears but "Open" isn't an option, press "Cancel" and repeat from step 1.
    The open button will appear the second time the warning appears.
