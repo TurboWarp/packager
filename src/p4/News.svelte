8a16ffe462758310b9b90b78d1bb347685509b5c
GarboMuffin <muffin@mailbox.org>
2022-12-04 12:46:36 -0600

    Remove News entries

--- src/p4/News.svelte
+++ src/p4/News.svelte
@@ -4,6 +4,7 @@
 </script>
 
 <style>
+  /*
   .badge {
     background-color: #b117f8;
     border-bottom: 2px solid #6f0073;
@@ -14,15 +15,13 @@
   ul {
     margin: 0;
   }
+  */
 </style>
 
+<!--
 <Section accent={color}>
   <div lang="en">
-    <p><span class="badge">New!</span> A lot of custom extension updates:</p>
-    <ul>
-      <li>Custom extensions are loaded unsandboxed in the packager</li>
-      <li>The packager automatically detects which custom extensions the project needs (requires recent version of TurboWarp, desktop app not updated yet)</li>
-      <li>Custom extensions will be downloaded and embedded into the packaged project so that it's self contained and works offline (doesn't work for all extensions, can be turned off in advanced settings)</li>
-    </ul>
+    <p><span class="badge">New!</span></p>
   </div>
 </Section>
+-->
