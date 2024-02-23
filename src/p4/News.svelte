604aacc9a52846f6657f55e608a1e6c8427d1c3f
Muffin <muffin@mailbox.org>
2024-02-23 15:43:49 -0600

    Open source should stay open source.

--- src/p4/News.svelte
+++ src/p4/News.svelte
@@ -4,7 +4,6 @@
 </script>
 
 <style>
-  /*
   .badge {
     background-color: #b117f8;
     border-bottom: 2px solid #6f0073;
@@ -12,16 +11,15 @@
     padding: 2px 5px;
     border-radius: 4px;
   }
-  ul {
-    margin: 0;
-  }
-  */
 </style>
 
-<!--
+
 <Section accent={color}>
   <div lang="en">
-    <p><span class="badge">New!</span></p>
+    <p><span class="badge">New!</span> (February 23, 2024)</p>
+    <p>We have changed the license of the packager and the TurboWarp runtime from the Apache 2.0 license to the GNU Lesser General Public License version 3.</p>
+    <p>You can still package closed-source or commercial projects <a href="https://docs.turbowarp.org/packager/commercial-use">the same as always</a>; this is just to ensure that forks of the packager itself have to be open source too.</p>
+    <p>If this is a problem for you, the previous version of the packager <a href="https://packager-legacy.turbowarp.org/">is still available</a>.</p>
   </div>
 </Section>
--->
+
--- src/packager/brand.js
+++ src/packager/brand.js
@@ -32,19 +32,19 @@ If this is set to an empty string, the link is removed.
 module.exports = {
   APP_NAME: 'TurboWarp Packager',
   WEBSITE: 'https://packager.turbowarp.org/',
-  COPYRIGHT_NOTICE: `Copyright (C) 2021-2022 Thomas Weber
+  COPYRIGHT_NOTICE: `Copyright (C) 2021-2024 Thomas Weber
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License version 3
+as published by the Free Software Foundation.
 
-    http://www.apache.org/licenses/LICENSE-2.0
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.`,
+See <https://www.gnu.org/licenses/> for a copy of the GNU Lesser
+General Public license.`,
   ACCENT_COLOR: '#ff4c4c',
   SOURCE_CODE: 'https://github.com/TurboWarp/packager',
   FEEDBACK_PRIMARY: {
