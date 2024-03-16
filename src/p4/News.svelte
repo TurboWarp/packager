43637cf7cb1470e7c99a5bd29cf07c075a6c1b23
Muffin <muffin@mailbox.org>
2024-03-15 21:11:45 -0500

    Mozilla Public License 2.0
    The GPL family of licenses are not perfect. Notably they are incompatible with
    the SIL OFL 1.1 used by the fonts in scratch-render-fonts. In the packager we
    'statically link' all of this those fonts into one HTML file. Is that legal?
    Seems like you can make an argument that it's not. Who knows. I'm not a lawyer.
    
    MPL 2.0 is a good alternative: It's still copyleft, used by real projects,
    backed by a real organization, and GPL compatible.
    
    Before 604aacc9a52846f6657f55e608a1e6c8427d1c3f all changes were either by me
    or under BSD3c. There are no third-party non-robot LGPL commits. Thus we can
    make this switch.
    
    Related: https://github.com/TurboWarp/scratch-gui/pull/909

--- src/p4/News.svelte
+++ src/p4/News.svelte
@@ -1,3 +1,4 @@
+<!--
 <script>
   import Section from './Section.svelte';
   const color = '#b117f8';
@@ -15,10 +16,6 @@
 
 <Section accent={color}>
   <div lang="en">
-    <p><span class="badge">New!</span> (February 28, 2024)</p>
-    <p>We have changed the license of the packager and the TurboWarp runtime from the Apache 2.0 license to the GNU Lesser General Public License version 3.</p>
-    <p>You can still package closed-source or commercial projects <a href="https://docs.turbowarp.org/packager/commercial-use">the same as always</a>; this is just to ensure that forks of the packager itself have to be open source too.</p>
-    <p>If this is a problem for you, the previous version of the packager <a href="https://packager-legacy.turbowarp.org/">is still available</a>.</p>
   </div>
 </Section>
-
+-->
