f2015ff4ad2e41c2cee4164155943b2ab8b28d7e
Thomas Weber <muffin@muffin.ink>
2025-10-02 16:43:17 -0500

    update news

--- src/p4/News.svelte
+++ src/p4/News.svelte
@@ -17,10 +17,12 @@
   <div lang="en">
     <p>
       <span class="badge">New!</span>
-      We've updated the packager to use the <a href="https://docs.turbowarp.org/new-compiler">new compiler</a>.
     </p>
     <p>
-      If you need the old compiler still, you can access it on <a href="https://packager-legacy.turbowarp.org/">packager-legacy.turbowarp.org</a>.
+      We've updated the packager to use the <a href="https://docs.turbowarp.org/new-compiler">new compiler</a> to make projects run even faster.
+    </p>
+    <p>
+      If your project relies on extensions that require the old compiler, a version of the packager using the old compiler will be available on <a href="https://packager-legacy.turbowarp.org/">packager-legacy.turbowarp.org</a> indefinitely.
     </p>
   </div>
 </Section>
